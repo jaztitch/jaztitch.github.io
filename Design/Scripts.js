fdf63864307116c7013c48b7e69ab86f89173a92
jaztitch <alterjaz@gmail.com>
2025-07-07 20:04:54 -0500

    partial reconstruction based on omniamors.neocities.orig

---
+++ Design/Scripts.js
@@ -0,0 +1,11 @@
+<script>
+var texts = [
+  "I am a sentence.",
+  "Some nice stuff.",
+  "I am random too!"
+];
+
+document.getElementById('randomText').value = texts[Math.floor(Math.random()*texts.length)];
+
+<input type="text" id="randomText"></input>
+</script>
